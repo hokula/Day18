c665a318f42c27f2fc67393ca2be50d04432848a
hokula <adelhokula18@gmail.com>
2021-03-26 10:50:39 +0100

    Set

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+let numberArray = new Set([9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5]);
+let uniqueArray = [];
+
+for (let item of numberArray) {
+  uniqueArray.push(item);
+}
+
+uniqueArray.sort((a, b) => a - b);
+console.log(uniqueArray);
+
+module.exports = { numberArray, uniqueArray };
