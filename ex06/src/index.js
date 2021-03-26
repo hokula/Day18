01be12918cab3d01591813e908284762ace4323d
hokula <adelhokula18@gmail.com>
2021-03-26 10:50:25 +0100

    Spreading Iterables

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+let rockStar = new Map([
+  ["artist", "The Rolling Stones"],
+  ["song", "Angie"],
+  ["album", "Goats Head Soup"],
+  ["singer", "Mick Jagger"],
+]);
+
+let newRock = new Map( [...rockStar].filter(([col]) => { return col[0] > "a"; }));
+console.log(newRock);
+
+module.exports = { rockStar, newRock };
